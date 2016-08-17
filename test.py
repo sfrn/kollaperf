26d8ca78d49ad4331c9fd12b4cfd5dcecb95d186
Friedrich Weber <fred@reichbier.de>
2016-08-17 21:06:50 +0200

    logging: use a rotating file handler

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
+from kollaperf.config import settings
 from kollaperf.machine import Machine
 
 formatter = logging.Formatter("%(asctime)s [%(name)s] [%(levelname)s] %(message)s")
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("kolla.log")
+file_handler = RotatingFileHandler("kolla.log", maxBytes=settings['log']['max_bytes'], backupCount=settings['log']['backup_count'])
 file_handler.setFormatter(formatter)
 root_logger.addHandler(file_handler)
 
