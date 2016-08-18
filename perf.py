6d7a1ae65a179de8b7977f3e827c2c5b4e8f47f4
Friedrich Weber <fred@reichbier.de>
2016-08-18 15:50:57 +0200

    Add automatic restart

--- perf.py
+++ perf.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+import sys, os
 import logging
+import threading
+import time
 from logging.handlers import RotatingFileHandler
 
 from kollaperf.config import settings
 from kollaperf.machine import Machine
+
+log = logging.getLogger(__name__)
+
+WAIT_UNTIL_RESTART= settings['restart_seconds']
+
+class WaitThread(threading.Thread):
+    def run(self):
+        time.sleep(WAIT_UNTIL_RESTART)
+        log.info('Kill me automatically ...')
+        os._exit(0)
 
 formatter = logging.Formatter("%(asctime)s [%(name)s] [%(levelname)s] %(message)s")
 root_logger = logging.getLogger()
@@ -16,6 +29,10 @@ root_logger.addHandler(file_handler)
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(formatter)
 root_logger.addHandler(consoleHandler)
+
+if len(sys.argv) > 1 and sys.argv[1] == '-r':
+    log.info('Kill after {} seconds ...'.format(WAIT_UNTIL_RESTART))
+    WaitThread().start()
 
 machine = Machine()
 machine.run_forever()
